e7ce09a96848531b4500e800fe43c1e87c299894
Ravi Kanani <ravikanani@mail.com>
2020-04-03 15:48:11 -0400

    Updates

---
+++ precache-manifest.c8a63bdfe70432202f9b1925c0bbe4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28e919f44a66f1139f995fc7f0b3d3d",
+    "url": "/myweather/index.html"
+  },
+  {
+    "revision": "11b08ff50ff92b607ac5",
+    "url": "/myweather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4c06ad790de71950d1a",
+    "url": "/myweather/static/js/2.050c6198.chunk.js"
+  },
+  {
+    "revision": "8c7fe4041954829e8f9a44c5426cd717",
+    "url": "/myweather/static/js/2.050c6198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b08ff50ff92b607ac5",
+    "url": "/myweather/static/js/main.dc95df9f.chunk.js"
+  },
+  {
+    "revision": "eec0a14bdaba1a1c8ad6",
+    "url": "/myweather/static/js/runtime-main.ad36ee91.js"
+  }
+]);
